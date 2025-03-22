7a5544fe7c1ea7e890e2ed35c46160be7780ada6
PersSeekJoy <nikitaolhovik14@gmail.com>
2025-03-22 09:24:19 +0300

    Create null_state.py

---
+++ states/null_state.py
@@ -0,0 +1,10 @@
+null_state: dict = {
+    "search": {},
+    "page_number": 1,
+    "in_searching": False,
+    "stars_fact_number": 0,
+    "planets_fact_number": 0,
+    "galaxies_fact_number": 0,
+    'actiive_category': '',
+    "nonsense_couner": 0
+}
