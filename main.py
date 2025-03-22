0987a90b14acf3918c2b3a40c887149c1dd71392
PersSeekJoy <nikitaolhovik14@gmail.com>
2025-03-22 09:07:13 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,36 @@
+import asyncio
+
+from aiogram import Bot, Dispatcher
+from aiogram.client.default import DefaultBotProperties
+from aiogram.enums import ParseMode
+from config_data import Config, load_config
+from handlers import other_handlers, user_handlers
+from keyboards import set_main_menu
+
+
+# Функция конфигурирования и запуска бота
+async def main():
+
+    # Загружаем конфиг в переменную config
+    config: Config = load_config()
+
+    # Инициализируем бот и диспетчер
+    bot = Bot(
+        token=config.tg_bot.token,
+        default=DefaultBotProperties(parse_mode=ParseMode.HTML)
+    )
+    dp = Dispatcher()
+
+    # Настраиваем главное меню бота
+    await set_main_menu(bot)
+
+    # Регистрируем роутеры в диспетчере
+    dp.include_router(user_handlers.router)
+    dp.include_router(other_handlers.router)
+
+    # Пропускаем накопившиеся апдейты и запускаем polling
+    await bot.delete_webhook(drop_pending_updates=True)
+    await dp.start_polling(bot)
+
+
+asyncio.run(main())
